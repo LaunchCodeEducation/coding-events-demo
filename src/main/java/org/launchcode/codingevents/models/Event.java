e32682dfc7d4fcebf56dc59c7c264c9c7b940dd9
Chris Bay <chris@chrisbay.net>
2019-12-12 11:48:37 -0600

    Adds description property to Event

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -7,17 +7,12 @@ import java.util.Objects;
  */
 public class Event {
 
-    private int id;
-    private static int nextId = 1;
-
     private String name;
     private String description;
 
     public Event(String name, String description) {
         this.name = name;
         this.description = description;
-        this.id = nextId;
-        nextId++;
     }
 
     public String getName() {
@@ -36,25 +31,9 @@ public class Event {
         this.description = description;
     }
 
-    public int getId() {
-        return id;
-    }
-
     @Override
     public String toString() {
         return name;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Event event = (Event) o;
-        return id == event.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
