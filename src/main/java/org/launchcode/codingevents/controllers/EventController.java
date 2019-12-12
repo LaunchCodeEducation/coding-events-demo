912127fdde10bccd556873e37d14a1d58a7bb8d0
Chris Bay <chris@chrisbay.net>
2019-12-12 11:50:33 -0600

    Removes description property

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -35,9 +35,8 @@ public class EventController {
     }
 
     @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName,
-                                         @RequestParam String eventDescription) {
-        events.add(new Event(eventName, eventDescription));
+    public String processCreateEventForm(@RequestParam String eventName) {
+        events.add(new Event(eventName));
         return "redirect:";
     }
 
--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -8,11 +8,9 @@ import java.util.Objects;
 public class Event {
 
     private String name;
-    private String description;
 
-    public Event(String name, String description) {
+    public Event(String name) {
         this.name = name;
-        this.description = description;
     }
 
     public String getName() {
@@ -21,14 +19,6 @@ public class Event {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     @Override
