1bdc6ad76753ffaec0dc7bd5453c0e14876592f7
Malathi416 <ammu.416@gmail.com>
2019-12-16 15:45:52 -0600

    added create scenario and property

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,10 +18,10 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+    private static List<Event> events = new ArrayList<>();
 
-    @GetMapping
-    public String displayAllEvents(Model model) {
+    @GetMapping // route
+    public String displayAllEvents(Model model) { //route handler method
         model.addAttribute("title", "All Events");
         model.addAttribute("events", events);
         return "events/index";
@@ -33,8 +34,8 @@ public class EventController {
     }
 
     @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName) {
-        events.add(eventName);
+    public String processCreateEventForm(@RequestParam String eventName,@RequestParam String eventDescription) {
+        events.add(new Event(eventName,eventDescription));
         return "redirect:";
     }
 
--- src/main/java/org/launchcode/codingevents/controllers/HomeController.java
+++ src/main/java/org/launchcode/codingevents/controllers/HomeController.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping
+    @GetMapping("")
     public String index() {
-        return "index";
+        return "home";
     }
 
 }
