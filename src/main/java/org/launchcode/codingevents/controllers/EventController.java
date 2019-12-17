70c9a779be63abf9c7d169c2c460b90e12b75a81
Malathi416 <ammu.416@gmail.com>
2019-12-17 14:55:41 -0600

    added a unique id

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -34,8 +34,8 @@ public class EventController {
     }
 
     @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName,@RequestParam String eventDescription) {
-        events.add(new Event(eventName,eventDescription));
+    public String processCreateEventForm(@RequestParam String eventName,@RequestParam String eventDescription,@RequestParam String eventLocation) {
+        events.add(new Event(eventName,eventDescription,eventLocation));
         return "redirect:";
     }
 
--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,11 +1,19 @@
 package org.launchcode.codingevents.models;
 
+import java.util.Objects;
+
 public class Event {
+    private int id;
+    private static int nextId = 1;
     private String name;
     private String description;
-    public Event(String name,String description) {
+    private String location;
+    public Event(String name,String description,String location) {
         this.name = name;
         this.description = description;
+        this.location = location;
+        this.id = nextId;
+        nextId++;
     }
 
     public String getName() {
@@ -24,8 +32,33 @@ public class Event {
         this.description = description;
     }
 
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getId() {
+        return id;
+    }
+
     @Override
     public String toString() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
