aee53d941de8fd2558829679d82aa4dd2b151389
Malathi416 <ammu.416@gmail.com>
2020-01-04 23:48:39 -0600

    added validations in controller to a validation-errors branch

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -4,8 +4,10 @@ import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,7 +32,13 @@ public class EventController {
     }
 
     @PostMapping("create")
-    public String processCreateEventForm(@ModelAttribute Event newEvent) {
+    public String processCreateEventForm(@ModelAttribute @Valid Event newEvent,
+                                         Errors errors,Model model) {
+        if(errors.hasErrors()){
+            model.addAttribute("title", "Create Event");
+            model.addAttribute("errorMsg","Bad Data!");
+            return "events/create";
+        }
         EventData.addEvent(newEvent);
         return "redirect:";
     }
