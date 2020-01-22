92947de30512ceac6c4eeba681bb963b2bae2c74
Malathi416 <ammu.416@gmail.com>
2020-01-22 15:38:00 -0600

    completed creating a persistent model

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,43 +1,48 @@
 package org.launchcode.codingevents.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Objects;
 
+@Entity
 public class Event {
+    @Id
+    @GeneratedValue
     private int id;
-    private static int nextId = 1;
+
     @NotBlank(message = "Name is required")
     @Size(min = 3,max = 50,message = "Name must be between 3 and 50 characters")
     private String name;
+
     @Size(max = 500 ,message = "Description is too long")
     private String description;
+
     @NotBlank(message="Email is required")
     @Email(message = "Invalid Email.Try again")
     private String contactEmail;
+
     @NotBlank(message = "Location is required")
-    @NotNull
     @Size(max = 50)
     private String location;
+
 //    @Size(min=1,max=5,message = "Atleast one person ")
     private int noOfPeople;
     private EventType type;
     public Event(String name,String description,String location,String contactEmail,int noOfPeople ,EventType type) {
-        this();
         this.name = name;
         this.description = description;
         this.location = location;
         this.contactEmail = contactEmail;
         this.noOfPeople = noOfPeople;
         this.type = type;
-//        this.id = nextId;
-//        nextId++;
     }
     public Event(){
-        this.id = nextId;
-        nextId++;
+
     }
     public String getName() {
         return name;
