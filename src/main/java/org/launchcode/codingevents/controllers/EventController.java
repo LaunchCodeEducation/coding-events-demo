eeb70c5a76eb1c739a024da3100c42530465c0ef
Chris Bay <chris@chrisbay.net>
2020-01-22 06:01:28 -0600

    Renames displayAllEvents > displayEvents

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -27,7 +27,7 @@ public class EventController {
     private EventCategoryRepository eventCategoryRepository;
 
     @GetMapping
-    public String displayAllEvents(@RequestParam(required = false) Integer categoryId, Model model) {
+    public String displayEvents(@RequestParam(required = false) Integer categoryId, Model model) {
 
         if (categoryId == null) {
             model.addAttribute("title", "All Events");
