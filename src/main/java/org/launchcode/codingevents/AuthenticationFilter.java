95040bb6c46d803cdfa6b39355984a66fc6ee8da
Chris Bay <chris@chrisbay.net>
2020-01-29 14:07:02 -0500

    Configures auth filter

---
+++ src/main/java/org/launchcode/codingevents/AuthenticationFilter.java
@@ -0,0 +1,62 @@
+package org.launchcode.codingevents;
+
+import org.launchcode.codingevents.controllers.AuthenticationController;
+import org.launchcode.codingevents.data.UserRepository;
+import org.launchcode.codingevents.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Chris Bay
+ */
+public class AuthenticationFilter extends HandlerInterceptorAdapter {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    AuthenticationController authenticationController;
+
+    private static final List<String> whitelist = Arrays.asList("/login", "/register", "/logout", "/css");
+
+    private static boolean isWhitelisted(String path) {
+        for (String pathRoot : whitelist) {
+            if (path.startsWith(pathRoot)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws IOException {
+
+        // Don't require sign-in for whitelisted pages
+        if (isWhitelisted(request.getRequestURI())) {
+            // returning true indicates that the request may proceed
+            return true;
+        }
+
+        HttpSession session = request.getSession();
+        User user = authenticationController.getUserFromSession(session);
+
+        // The user is logged in
+        if (user != null) {
+            return true;
+        }
+
+        // The user is NOT logged in
+        response.sendRedirect("/login");
+        return false;
+    }
+
+}
---
+++ src/main/java/org/launchcode/codingevents/WebApplicationConfig.java
@@ -0,0 +1,26 @@
+package org.launchcode.codingevents;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by Chris Bay
+ */
+@Configuration
+public class WebApplicationConfig implements WebMvcConfigurer {
+
+    // Create spring-managed object to allow the app to access our filter
+    @Bean
+    public AuthenticationFilter authenticationFilter() {
+        return new AuthenticationFilter();
+    }
+
+    // Register the filter with the Spring container
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor( authenticationFilter() );
+    }
+
+}
