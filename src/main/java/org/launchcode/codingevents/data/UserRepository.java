097c03d0a2f2642652680b861c6bc0a4e5551366
Chris Bay <chris@chrisbay.net>
2020-01-29 13:58:55 -0500

    Adds user model

---
+++ src/main/java/org/launchcode/codingevents/data/UserRepository.java
@@ -0,0 +1,13 @@
+package org.launchcode.codingevents.data;
+
+import org.launchcode.codingevents.models.User;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Chris Bay
+ */
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    User findByUsername(String username);
+
+}
