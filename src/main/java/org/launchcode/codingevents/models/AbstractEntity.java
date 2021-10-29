83e6e2e3a5eb1e8e25fc4387d145d32f0c1c8b09
aspiezebra <81698471+aspiezebra@users.noreply.github.com>
2021-10-29 11:59:02 -0500

    AbstractEntity

---
+++ src/main/java/org/launchcode/codingevents/models/AbstractEntity.java
@@ -0,0 +1,32 @@
+package org.launchcode.codingevents.models;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.util.Objects;
+
+
+@MappedSuperclass
+public abstract class  AbstractEntity {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity that = (AbstractEntity) o;
+        return getId() == that.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
