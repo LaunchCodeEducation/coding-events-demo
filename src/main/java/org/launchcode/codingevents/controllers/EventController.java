582823ad257caa28fbb5b2d02698e14855f8567c
Kavitha <kavithamsca@gmail.com>
2023-07-31 17:03:45 -0500

    completed exersice

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -57,4 +57,21 @@ public class EventController {
         }
         return "redirect";
     }
+    @GetMapping("edit/{eventId}")
+    public String displayEditForm(Model model, @PathVariable int eventId) {
+        Event eventToEdit = EventData.getById(eventId);
+        model.addAttribute("event", eventToEdit);
+        String tittle = "Edit Event " + eventToEdit.getName() + " (id=" + eventToEdit.getId() +")";
+        model.addAttribute("tittle", tittle);
+        return "events/edit";
+        // controller code will go here
+    }
+    @PostMapping("edit")
+    public String processEditForm(int eventId, String name, String description) {
+        Event eventToEdit = EventData.getById(eventId);
+        eventToEdit.setName(name);
+        eventToEdit.setDescription(description);
+        return "redirect:";
+    }
+
 }
